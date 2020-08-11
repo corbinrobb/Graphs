4d56997ba95773c6f33941ed1b152ab1f0f7307a
Corbin Robb <corbin@Corbins-MacBook-Pro.local>
2020-08-11 10:11:40 -0600

    Meet Day 2 and Day 3 MVP

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,19 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    d = {}
+
+    for node in ancestors:
+        if node[1] not in d:
+            d[node[1]] = [node[0]]
+        else:
+            d[node[1]].append(node[0])
+
+    cur = starting_node
+
+    if cur not in d:
+        return -1
+
+    while cur in d:
+        cur = d[cur][0]
+
+    return cur
